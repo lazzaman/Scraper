3c6f75e121ed755fdf794bacbbfb68053030faae
mazzutti <tiago.mazzutti@ifc.edu.br>
2019-10-08 15:18:17 -0300

    final version!

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
-from flask import Flask, render_template, request
-from scraping import scrape
+from flask import Flask, render_template, request, send_from_directory
 from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('static/js', path)
+
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('static/css', path)
 
 class Hello(Resource):
-    def post(self):
-        return render_template("index.html")
+    def get(self):
+        return render_template("hello.html")
 
+class Notas(Resource):
+    def get(self):
+        return render_template("notas.html")
+
+class Faltas(Resource):
+    def get(self):
+        return render_template("faltas.html")
 
 api.add_resource(Hello, '/hello')
-
-
+api.add_resource(Notas, '/notas')
+api.add_resource(Faltas, '/faltas')
 
 @app.route('/')
 def index():
