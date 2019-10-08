4304ca028e341770a91573fbbaaeb4c2263adfdc
mazzutti <tiago.mazzutti@ifc.edu.br>
2019-10-08 09:47:41 -0300

    initial post request implementation

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from flask import jsonify
 from scraping import scrape
+from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
+api = Api(app)
 
+class Login(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('User-Agent', location='headers')
+        args = parser.parse_args()
+        return jsonify(data = scrape(args))
 
-@app.route('/')
-def index():
-    return jsonify(Products=scrape())
-
+api.add_resource(Login, '/login')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
