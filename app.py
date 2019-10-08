e4d697d6c331385331a818c73f89a425a9805042
Mazzutti <tiagomzt@gmail.com>
2019-10-08 13:36:25 -0300

    Simplify!

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
-from flask import Flask
-from flask import jsonify
+from flask import Flask, render_template, request
 from scraping import scrape
 from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
-class Login(Resource):
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('User-Agent', location='headers')
-        args = parser.parse_args()
-        return jsonify(data = scrape(args))
 
-api.add_resource(Login, '/login')
+class Hello(Resource):
+    def post(self):
+        return render_template("index.html")
+
+
+api.add_resource(Hello, '/hello')
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
