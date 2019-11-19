c8d66818bcb199a2b9bb589aa4ff31fdb2ee6d82
LUIZ HENRIQUE LAZZARIN <2018305339@ifc-48925-linux.labinfo.concordia.ifc.edu.br>
2019-11-19 08:05:00 -0200

    cleanup...

--- app.py
+++ app.py
@@ -4,33 +4,15 @@ from flask_restful import Resource, Api, reqparse
 app = Flask(__name__)
 api = Api(app)
 
-@app.route('/js/<path:path>')
-def send_js(path):
-    return send_from_directory('static/js', path)
-
-@app.route('/css/<path:path>')
-def send_css(path):
-    return send_from_directory('static/css', path)
-
-class Hello(Resource):
+class Ola(Resource):
     def get(self):
-        return render_template("hello.html")
-
-class Notas(Resource):
-    def get(self):
-        return render_template("notas.html")
-
-class Faltas(Resource):
-    def get(self):
-        return render_template("faltas.html")
+        return render_template("ola.html")
 
 api.add_resource(Hello, '/hello')
-api.add_resource(Notas, '/notas')
-api.add_resource(Faltas, '/faltas')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('hello.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
